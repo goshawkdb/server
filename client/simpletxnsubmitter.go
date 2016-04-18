b4d8ef996c135c79263d98ebf5d728f71f2813c1
Matthew Sackman <matthew@goshawkdb.io>
2016-04-18 16:50:49 +0100

    Actually block client-submitted txns during topology changes. Ref T6.
    --HG--
    branch : T6

--- client/simpletxnsubmitter.go
+++ client/simpletxnsubmitter.go
@@ -136,7 +136,7 @@ func (sts *SimpleTxnSubmitter) SubmitTransaction(txnCap *msgs.Txn, activeRMs []c
 
 func (sts *SimpleTxnSubmitter) SubmitClientTransaction(ctxnCap *cmsgs.ClientTxn, continuation TxnCompletionConsumer, delay time.Duration, useNextVersion bool) error {
 	// Frames could attempt rolls before we have a topology.
-	if sts.topology.IsBlank() {
+	if sts.topology.IsBlank() || (!useNextVersion && sts.topology.Next() != nil) {
 		fun := func() { sts.SubmitClientTransaction(ctxnCap, continuation, delay, useNextVersion) }
 		if sts.bufferedSubmissions == nil {
 			sts.bufferedSubmissions = []func(){fun}
