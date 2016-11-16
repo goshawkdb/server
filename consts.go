4963d12e25922aa015278d0ee04f9f277448a577
Matthew Sackman <matthew@goshawkdb.io>
2016-11-16 11:42:24 +0000

    Set version. Ref T52
    --HG--
    branch : T52

--- consts.go
+++ consts.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	ServerVersion                 = "dev"
+	ServerVersion                 = "0.3"
 	MDBInitialSize                = 1048576
 	TwoToTheSixtyThree            = 9223372036854775808
 	SubmissionMinSubmitDelay      = 2 * time.Millisecond
